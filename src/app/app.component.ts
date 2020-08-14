5d0cc6e1da9c68a4c773e9d43a710975b4d7a63c
Ariel Martin <arielmartin80@gmail.com>
2020-08-14 19:42:00 -0300

    se agrego item CRUD al nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirstapp';
+
+  name: string = 'Jhon Carter';
+  age: number = 28;
+ 
+  users: string[] = ['ryan', 'joe', 'cameron'];
+
+  constructor(private dataService: DataService) {
+    this.dataService.getData().subscribe(data => {
+      this.posts = data;
+    });
+  }
+
+sayHello(){
+  alert('Hello');
+}
+
+addUser(newUser) {
+  this.users.push(newUser.value);
+  newUser.value = '';
+  newUser.focus();
+  return false;
+}
+
+deleteUser(user) {
+  for(let i = 0; i < this.users.length; i++) {
+    if( user == this.users[i] ){
+      this.users.splice(i, 1);
+    }
+  }
+
+}
+
+posts = [];
+
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Post } from './Post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) {
+    console.log('Service is Working!');
+  }
+
+   getData() {
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+  }
+
+}
+
+
+
