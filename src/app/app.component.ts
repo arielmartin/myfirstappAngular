3f7a13b5e112b61a2bea23b83ffd85e5102276e2
Ariel Martin <arielmartin80@gmail.com>
2020-08-14 21:25:03 -0300

    CRUD actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,25 +20,9 @@ export class AppComponent {
     });
   }
 
-sayHello(){
-  alert('Hello');
-}
 
-addUser(newUser) {
-  this.users.push(newUser.value);
-  newUser.value = '';
-  newUser.focus();
-  return false;
-}
 
-deleteUser(user) {
-  for(let i = 0; i < this.users.length; i++) {
-    if( user == this.users[i] ){
-      this.users.splice(i, 1);
-    }
-  }
 
-}
 
 posts = [];
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,7 +8,45 @@ import { Component, OnInit, Input } from '@angular/core';
 export class UserComponent implements OnInit {
   @Input() nameUser;
 
+  user: string = '';
+  lastUser: string = '';
+  users: string[] = ['ryan', 'joe', 'cameron'];
+
   constructor() { }
+
+  sayHello(user){
+    alert('Hello ' + user);
+  }
+
+  addUser(newUser) {
+    this.users.push(newUser.value);
+    newUser.value = '';
+    newUser.focus();
+    return false;
+  }
+  
+  deleteUser(user) {
+    for(let i = 0; i < this.users.length; i++) {
+      if( user == this.users[i] ){
+        this.users.splice(i, 1);
+      }
+    }
+  }
+
+  chargeInput(user){
+    this.user = user;
+    this.lastUser = user;
+  }
+
+  updateUser(modUser) {
+    if (modUser.value) {
+      console.log(this.lastUser);
+      this.deleteUser(this.lastUser);
+      this.addUser(modUser);
+      return false;
+    }
+  }
+
 
   ngOnInit(): void {
   }
